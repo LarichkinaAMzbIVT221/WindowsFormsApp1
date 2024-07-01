e66306a63d101c022bc652c58072859759c63c60
ASUS <ASUS@DESKTOP-MUGET55>
2024-07-01 21:40:09 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Прямоугольник class1 = new Прямоугольник(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+            MessageBox.Show("Периметр равен" + class1.Периметр());
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form3.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Прямоугольник class1 = new Прямоугольник(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+            MessageBox.Show("Площадь равна" + class1.Площадь());
+        }
+    }
+}
